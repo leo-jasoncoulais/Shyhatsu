cae1a816b3d359efee342aaf44037c9463cec019
Léo Jason Coulais <leo.jascoulais@gmail.com>
2024-12-31 15:42:44 +0100

    Remove token from main file...

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import nextcord as nc
 from nextcord.ext import commands
-from os import listdir
+from os import listdir, getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bot = commands.Bot(intents=nc.Intents.all())
 
@@ -16,4 +19,4 @@ for C in listdir("cogs"):
 
 bot.load_extensions(cogs)
 
-bot.run("MTI4NzQ0MzYzNzA0Njg3MDAxNg.G8OxkW.Oxy9V2aOx0-nun2pHL3ZdwsSQyKC1I0oBVxgWo")
+bot.run(getenv("TOKEN"))
