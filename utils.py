c3c75d2d6924fc609ba1071a063091b101388ed3
Léo Jason Coulais <leo.jascoulais@gmail.com>
2025-01-15 15:22:31 +0100

    [+] Add utils file

---
+++ utils.py
@@ -0,0 +1,20 @@
+
+from json import load, dump
+
+class Config:
+
+    def __init__(self):
+        self.file = "config.json"
+
+    def get_config(self):
+        with open(self.file, "r") as file:
+            return load(file)
+
+    def get_value(self, key):
+        return self.get_config()[key]
+        
+    def set_value(self, key, value):
+        with open(self.file, "w") as file:
+            config = self.get_config()
+            config[key] = value
+            dump(config, file, indent=4)
